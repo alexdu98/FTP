925991dc49768b0087e5706a9922ca15ce79f2a6
Vincent Iampietro <vincent.iampietro@outlook.fr>
2016-11-27 20:28:17 +0100

    Travail sur la fonctionnalite GETLIST, parcours du storage dir. Pb sur la construction d'un tableau de char*. Les pointeurs ne peuevent pas etre transporter a travers le reseau.

--- partie1/client.c
+++ partie1/client.c
@@ -1,184 +1,197 @@
-// gcc -o client client.c
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netdb.h>
-#include <libgen.h>
-
-#define BEGIN 0
-#define GETLIST 1
-#define GET 2
-
-#define SIZE_BUF_CMD 2048
-
-struct msg{
-	int cmd;
-	int msg_size;
-	char content[256];
-};
+#include "client.h"
 
 void afficherCommandes(){
-	printf("\nCommandes disponibles : \n");
-	printf("----------------------- \n\n");
-	printf("GETLIST \n - Retourne la liste des fichiers telechargeables du serveur \n\n");
-	printf("GET <nomFichier> [-DIRL repertoireLocal] \n - Telecharge le fichier nomFichier dans repertoireLocal (./ si vide) \n\n");
-	printf("QUIT \n - Quitte le programme \n\n");
+  printf("\nCommandes disponibles : \n");
+  printf("----------------------- \n\n");
+  printf("GETLIST \n - Retourne la liste des fichiers telechargeables du serveur \n\n");
+  printf("GET <nomFichier> [-DIRL repertoireLocal] \n - Telecharge le fichier nomFichier dans repertoireLocal (./ si vide) \n\n");
+  printf("QUIT \n - Quitte le programme \n\n");
 }
 
 int main(int argc, char **argv){
 
-	if(argc != 3){
-		printf("Usage : client IPServeur|NDDServeur PortServeur \n");
-		return EXIT_FAILURE;
+  if(argc != 3){
+    printf("Usage : client IPServeur|NDDServeur PortServeur \n");
+    return EXIT_FAILURE;
+  }
+
+  /*
+    Récupère l'IP à partir d'un nom de domaine
+  */
+  char *realIP;
+  struct hostent *host;
+  if((host = gethostbyname(argv[1])) == NULL){
+    perror("Erreur gethostbyname() ");
+    return EXIT_FAILURE;
+  }
+  struct in_addr **ip = (struct in_addr **) host->h_addr_list;
+  realIP = inet_ntoa(*ip[0]);
+
+  /*
+    Création de la socket TCP
+  */
+  int localSocket;
+  if((localSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    perror("Erreur socket() ");
+    return EXIT_FAILURE;
+  }
+
+  /*
+    Connexion à la socket
+  */
+  struct sockaddr_in clientAddr;
+  clientAddr.sin_family = AF_INET;
+  clientAddr.sin_addr.s_addr = inet_addr(realIP);
+  clientAddr.sin_port = htons(atoi(argv[2]));
+  if(connect(localSocket, (struct sockaddr *) &clientAddr, sizeof clientAddr) == -1){
+    perror("Erreur connect() ");
+    return EXIT_FAILURE;
+  }
+
+  printf("En attente de connexion... \n");
+
+  struct msg msg;
+  int sizeRcvTotal = 0;
+  int sizeRcv = 0;
+
+  do{
+    if((sizeRcv = recv(localSocket, &msg, sizeof(msg) - sizeRcvTotal, 0)) == -1){
+      perror("Erreur recv() ");
+      close(localSocket);
+      return EXIT_FAILURE;
+    }
+    sizeRcvTotal += sizeRcv;
+    printf("size rcv total = %d\n", sizeRcvTotal);
+  } while(sizeRcvTotal < sizeof(msg.msg_size));
+
+  printf("msg_size = %lu\n", sizeof(msg.msg_size));
+  
+  while(sizeRcvTotal < msg.msg_size) {
+    if((sizeRcv = recv(localSocket, &msg, sizeof(msg) - sizeRcvTotal, 0)) == -1){
+      perror("Erreur recv() ");
+      close(localSocket);
+      return EXIT_FAILURE;
+    }
+    sizeRcvTotal += sizeRcv;
+    
+  } 
+  
+  if(msg.cmd != BEGIN){
+    printf("Connexion refusee, veuillez reessayer plus tard \n");
+    close(localSocket);
+    return EXIT_FAILURE;
+  }
+
+  printf("Connexion acceptee \n");
+
+  char cmd[SIZE_BUF_CMD];
+  int sizeSendTotal = 0;
+  int sizeSend;
+
+  while(1){
+
+    memset(cmd, 0, sizeof(cmd));
+
+    printf("\nEntrez une commande (HELP pour avoir la liste) : \n");
+
+    fgets(cmd, sizeof(cmd), stdin);
+    if(cmd[0] == '\n') break;
+    cmd[strlen(cmd) - 1] = '\0';
+
+    if(strcmp(cmd, "HELP") == 0){
+
+      afficherCommandes();
+
+    }
+    else if(strcmp(cmd, "QUIT") == 0){
+
+      break;
+
+    }
+    else if(strcmp(cmd, "GETLIST") == 0){
+      
+      msg.cmd = GETLIST;
+      msg.msg_size = sizeof(msg);
+
+      // ENVOI DE LA CMD GETLIST
+      while(sizeSendTotal < sizeof(msg)) {
+	if((sizeSend = send(localSocket, &msg, sizeof(msg) - sizeSendTotal , 0)) == -1){
+	  perror("Erreur send() ");
+	}
+	sizeSendTotal += sizeSend;
+      }
+
+      // RECEPTION DU RESULTAT DE GETLIST
+      sizeRcvTotal = 0;
+      do{
+	if((sizeRcv = recv(localSocket, &msg, sizeof(msg) - sizeRcvTotal, 0)) == -1){
+	  perror("Erreur recv() ");
+	  close(localSocket);
+	  return EXIT_FAILURE;
+	}
+	sizeRcvTotal += sizeRcv;
+	printf("size rcv total = %d\n", sizeRcvTotal);
+      } while(sizeRcvTotal < sizeof(msg.msg_size));
+
+      printf("msg_size = %d", msg.msg_size);
+      
+      while(sizeRcvTotal < msg.msg_size) {
+	if((sizeRcv = recv(localSocket, &msg, sizeof(msg) - sizeRcvTotal, 0)) == -1){
+	  perror("Erreur recv() ");
+	  close(localSocket);
+	  return EXIT_FAILURE;
+	}
+	sizeRcvTotal += sizeRcv;
+      } 
+
+      // AFFICHAGE DU RESULTAT
+      for(int i = 0; i < msg.content.file_list.file_nb; i++) {
+	printf("%s\n", (msg.content.file_list.list)[i]);
+      }
+
+      
+    }
+    else if(strspn(cmd, "GET") == 3){
+
+      msg.cmd = GET;
+      
+      char* tableauNomsFichiers = strtok(cmd, " ");
+
+      char DIRLocal[256] = "./";
+
+      while(tableauNomsFichiers != NULL){
+	if(strcmp(tableauNomsFichiers, "-DIRL") == 0){
+	  tableauNomsFichiers = strtok(NULL, " ");
+	  if(tableauNomsFichiers != NULL){
+	    strcpy(DIRLocal, tableauNomsFichiers);
+	  }
+	  else{
+	    printf("[-DIRL repertoireLocal] \n");
+	    continue;
+	  }
 	}
 
-	/*
-		Récupère l'IP à partir d'un nom de domaine
-	*/
-	char *realIP;
-	struct hostent *host;
-	if((host = gethostbyname(argv[1])) == NULL){
-		perror("Erreur gethostbyname() ");
-		return EXIT_FAILURE;
+	tableauNomsFichiers = strtok(NULL, " ");
+      }
+
+      do{
+	if((sizeSend = send(localSocket, cmd, strlen(cmd), 0)) == -1){
+	  perror("Erreur send() ");
+	  continue;
 	}
-	struct in_addr **ip = (struct in_addr **) host->h_addr_list;
-	realIP = inet_ntoa(*ip[0]);
+	sizeSendTotal += sizeSend;
+      }while(sizeSendTotal < sizeof(msg.cmd));
 
-	/*
-		Création de la socket TCP
-	*/
-	int localSocket;
-	if((localSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-		perror("Erreur socket() ");
-		return EXIT_FAILURE;
-	}
+    }
+    else{
 
-	/*
-		Connexion à la socket
-	*/
-	struct sockaddr_in clientAddr;
-	clientAddr.sin_family = AF_INET;
-	clientAddr.sin_addr.s_addr = inet_addr(realIP);
-	clientAddr.sin_port = htons(atoi(argv[2]));
-	if(connect(localSocket, (struct sockaddr *) &clientAddr, sizeof clientAddr) == -1){
-		perror("Erreur connect() ");
-		return EXIT_FAILURE;
-	}
+      printf("Commande inconnue \n");
 
-	printf("En attente de connexion... \n");
+    }
 
-	struct msg msg;
-	int sizeRcvTotal = 0;
-	int sizeRcv;
-	do{
-		if((sizeRcv = recv(localSocket, &msg, sizeof(msg), 0)) == -1){
-			perror("Erreur recv() ");
-			close(localSocket);
-			return EXIT_FAILURE;
-		}
-		sizeRcvTotal += sizeRcv;
-	}while(sizeRcvTotal < sizeof(msg.cmd));
+  }
 
-	if(msg.cmd != BEGIN){
-		printf("Connexion refusee, veuillez reessayer plus tard \n");
-		close(localSocket);
-		return EXIT_FAILURE;
-	}
+  close(localSocket);
 
-	printf("Connexion acceptee \n");
-
-	char cmd[SIZE_BUF_CMD];
-	int sizeSendTotal = 0;
-	int sizeSend;
-
-	while(1){
-
-		memset(cmd, 0, sizeof(cmd));
-
-		printf("\nEntrez une commande (HELP pour avoir la liste) : \n");
-
-		fgets(cmd, sizeof(cmd), stdin);
-		if(cmd[0] == '\n') break;
-		cmd[strlen(cmd) - 1] = '\0';
-
-		if(strcmp(cmd, "HELP") == 0){
-
-			afficherCommandes();
-
-		}
-		else if(strcmp(cmd, "QUIT") == 0){
-
-			break;
-
-		}
-		else if(strcmp(cmd, "GETLIST") == 0){
-
-			msg.cmd = GETLIST;
-
-			do{
-				if((sizeSend = send(localSocket, cmd, strlen(cmd), 0)) == -1){
-					perror("Erreur send() ");
-					continue;
-				}
-				sizeSendTotal += sizeSend;
-			}while(sizeSendTotal < sizeof(msg.cmd));
-
-			do{
-				if((sizeRcv = recv(localSocket, &msg, sizeof(msg), 0)) == -1){
-					perror("Erreur recv() ");
-					close(localSocket);
-					return EXIT_FAILURE;
-				}
-				sizeRcvTotal += sizeRcv;
-			}while(sizeRcvTotal < sizeof(msg.cmd));
-
-		}
-		else if(strspn(cmd, "GET") == 3){
-
-			msg.cmd = GET;
-
-			tableauNomsFichiers = strtok(cmd, " ");
-
-			char DIRLocal[256] = "./";
-
-			while(tableauNomsFichiers != NULL){
-				if(strcmp(tableauNomsFichiers, "-DIRL") == 0){
-					tableauNomsFichiers = strtok(NULL, " ");
-					if(tableauNomsFichiers != NULL){
-						strcpy(DIRLocal, tableauNomsFichiers);
-					}
-					else{
-						printf("[-DIRL repertoireLocal] \n");
-						continue;
-					}
-				}
-
-				tableauNomsFichiers = strtok(NULL, " ");
-			}
-
-			do{
-				if((sizeSend = send(localSocket, cmd, strlen(cmd), 0)) == -1){
-					perror("Erreur send() ");
-					continue;
-				}
-				sizeSendTotal += sizeSend;
-			}while(sizeSendTotal < sizeof(msg.cmd));
-
-		}
-		else{
-
-			printf("Commande inconnue \n");
-
-		}
-
-	}
-
-	close(localSocket);
-
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
--- partie1/serveur.c
+++ partie1/serveur.c
@@ -1,69 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <pwd.h>
-#include <netdb.h>
-
-#include <signal.h>
-#include <ctype.h>
-
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-#include <string.h>
-
-// Pour les threads
-#include <pthread.h>
-#include <sys/syscall.h>
-
-#define LG_FILE_ATTENTE 1
-#define PATH_TO_STORAGE_DIR "./files/"
-#define BEGIN 0
-
-struct msg {
-	int cmd;
-	int content_size;
-	char content[256];
-};
-
+#include "serveur.h"
 
 /***********
  *** MAIN
  ***********/
 int main(int argc, char* argv[]) {
 
-	int go = 1;
-	char r_buffer[256];
 
   /**********************************************************************************
    ********************    MISE EN PLACE DE LA COMMUNICATION     ********************
    **********************************************************************************/
 	
   // Creation de la boite publique
-	int fd_brPublique = socket(PF_INET, SOCK_STREAM, 0);
-	if(fd_brPublique == -1) {
-		perror("socket ");
-		exit(EXIT_FAILURE);
-	}
+  int fd_brPublique = socket(PF_INET, SOCK_STREAM, 0);
+  if(fd_brPublique == -1) {
+    perror("socket ");
+    exit(EXIT_FAILURE);
+  }
 
-	struct sockaddr_in brPublique;
-	brPublique.sin_family = AF_INET;
-	brPublique.sin_addr.s_addr = INADDR_ANY;
+  struct sockaddr_in brPublique;
+  brPublique.sin_family = AF_INET;
+  brPublique.sin_addr.s_addr = INADDR_ANY;
   brPublique.sin_port = 0; // num de port choisi par le systeme
 
   // Liaison de la socket et de la struct 
   int ret_bind = bind(fd_brPublique, (struct sockaddr *) &brPublique, sizeof(brPublique));
   if(ret_bind == -1) {
-	perror("bind ");
-	exit(EXIT_FAILURE);
+    perror("bind ");
+    exit(EXIT_FAILURE);
   }
 
   // Afficher le numero de port pour les clients
@@ -75,16 +38,21 @@ int main(int argc, char* argv[]) {
   // Creation d'une file d'attente des connexions
   int ret_listen = listen(fd_brPublique, LG_FILE_ATTENTE);
   if(ret_listen == -1) {
-	perror("listen ");
-	exit(EXIT_FAILURE);
+    perror("listen ");
+    exit(EXIT_FAILURE);
   }
 
   /****************************************************************
    ***************    TRAITEMENT DU CLIENT     ********************
    ****************************************************************/
   
+  int go = 1;
   int ret_shutdown = 0, ret_send = 0, ret_recv = 0, ret_close = 0;
+  int r_total_size = 0, s_total_size = 0;
   int nb_clients = 0;
+
+  struct msg m_send;
+  struct msg m_recv;
   
   struct sockaddr_in brCv;
   socklen_t lgbrCv = sizeof(brCv);
@@ -94,75 +62,143 @@ int main(int argc, char* argv[]) {
   
   while(go) {
 
-	// Acceptation d'une connexion de client
-	fd_circuitV = accept(fd_brPublique, (struct sockaddr * ) &brCv, &lgbrCv);
+    // Acceptation d'une connexion de client
+    fd_circuitV = accept(fd_brPublique, (struct sockaddr * ) &brCv, &lgbrCv);
 	
-	// Augmente le nb de clients apres acceptation
-	nb_clients++;
+    // Augmente le nb de clients apres acceptation
+    nb_clients++;
 	
-	if(fd_circuitV == -1) {
-		perror("accept ");
-	}
-	// Traitement normal du client
-	else { 
-		printf("Un client vient de se connecter. FD = %d\n", fd_circuitV);
-		online = 1;
-
-	  // Envoi message au client pour commencer les transactions
-		struct msg m_send;
-		m_send.cmd = BEGIN;
-		m_send.content_size = 0;
-		
-		while (ret_send < sizeof(m_send)) {
+    if(fd_circuitV == -1) {
+      perror("accept ");
+    }
+    // Traitement normal du client
+    else { 
+      printf("Un client vient de se connecter. FD = %d\n", fd_circuitV);
+      online = 1;
+      
+      // Envoi message au client pour commencer les transactions
+      m_send.msg_size = sizeof(BEGIN);
+      m_send.cmd = BEGIN;
+      
+      // Tant que tout le msg n'est pas envoye, on boucle
+      while (s_total_size < sizeof(m_send)) {
 			
-			ret_send += send(fd_circuitV, &m_send + ret_send, sizeof(m_send) - ret_send, 0);
-			if(ret_send == -1) {
-				perror("send begin ");
-			}
-		}
-		
-	  // BOUCLE DE RECEPTION DES COMMANDES
-		while(online) {
-			ret_recv = recv(fd_circuitV, r_buffer, sizeof(r_buffer), 0);
-
-			if(ret_recv == -1) {
-				perror("recv cmd ");
-				
-			}
+	ret_send = send(fd_circuitV, &m_send, sizeof(m_send) - s_total_size, 0);
+	if(ret_send == -1) {
+	  perror("send begin ");
+	}
 	// Si le client se deconnecte intempestivement
-			else if(ret_recv == 0) {   
-				ret_shutdown = shutdown(fd_circuitV, SHUT_RDWR);
-				if(ret_shutdown == -1) perror("shutdown disconnect ");
+	else if(ret_send == 0) {   
+	  ret_shutdown = shutdown(fd_circuitV, SHUT_RDWR);
+	  if(ret_shutdown == -1) perror("shutdown disconnect ");
 
 	  // Ferme le descripteur de la socket
-				ret_close = close(fd_circuitV);
-				if(ret_close == -1) perror("close ");
+	  ret_close = close(fd_circuitV);
+	  if(ret_close == -1) perror("close ");
 				
-				nb_clients--;
-				online = 0;
-			}
-			else {
-				r_buffer[ret_recv] = '\0';
-				
-				char* cmd_buffer = strdup(r_buffer);
-				if(cmd_buffer == NULL) perror("strdup ");
-
-				char delim = ' ';
-				char* token = strsep(&cmd_buffer, &delim);
-
-				printf("Cmd received : %s\n", token);
-				
-				if(strcmp(token, "GETLIST") == 0) {
-		/* int fd_storage_dir = open(PATH_TO_STORAGE_DIR, ) */
-				}
-				else {
-					
-				}
-			}
-			
-		}
-		
+	  nb_clients--;
+	  online = 0;
 	}
+
+	s_total_size += ret_send;
+      }
+      
+      // BOUCLE DE RECEPTION DES COMMANDES
+      while(online) {
+
+	// Reception des 4 premiers octets contenant la taille du msg
+	while (r_total_size < sizeof(m_send.msg_size)) {
+	  ret_recv = recv(fd_circuitV, &m_recv, sizeof(m_recv) - r_total_size, 0);
+
+	  if(ret_recv == -1) {
+	    perror("recv cmd ");
+	    exit(EXIT_FAILURE);
+	  }
+	  // Si le client se deconnecte intempestivement
+	  else if(ret_recv == 0) {   
+	    ret_shutdown = shutdown(fd_circuitV, SHUT_RDWR);
+	    if(ret_shutdown == -1) perror("shutdown disconnect ");
+
+	    // Ferme le descripteur de la socket
+	    ret_close = close(fd_circuitV);
+	    if(ret_close == -1) perror("close ");
+				
+	    nb_clients--;
+	    online = 0;
+	  }
+	  
+	  r_total_size += ret_recv;
+	}
+
+
+	// Taille du msg recu, maintenant reception du reste
+	while(r_total_size < m_recv.msg_size) {
+	  ret_recv = recv(fd_circuitV, &m_recv, sizeof(m_recv) - r_total_size, 0);
+
+	  if(ret_recv == -1) {
+	    perror("recv cmd ");
+	    exit(EXIT_FAILURE);
+	  }
+	  // Si le client se deconnecte intempestivement
+	  else if(ret_recv == 0) {   
+	    ret_shutdown = shutdown(fd_circuitV, SHUT_RDWR);
+	    if(ret_shutdown == -1) perror("shutdown disconnect ");
+
+	    // Ferme le descripteur de la socket
+	    ret_close = close(fd_circuitV);
+	    if(ret_close == -1) perror("close ");
+				
+	    nb_clients--;
+	    online = 0;
+	  }
+
+	  r_total_size += ret_recv;
+	}
+
+	/* TRAITEMENT DE LA CMD ET RENVOI DU RESULTAT */
+	switch(m_recv.cmd) {
+	case GETLIST :
+	  printf("Cmd GETLIST\n");
+	  struct f_list* list = listdir(PATH_TO_STORAGE_DIR);
+	  if(list == NULL) perror("listdir serveur ");
+
+	  m_send.cmd = GETLIST;
+	  m_send.content.file_list = *list;
+	  m_send.msg_size = sizeof(m_send);
+
+	  s_total_size = 0;
+	  while (s_total_size < sizeof(m_send)) {
+			
+	    ret_send = send(fd_circuitV, &m_send, sizeof(m_send) - s_total_size, 0);
+	    if(ret_send == -1) {
+	      perror("send begin ");
+	    }
+	    // Si le client se deconnecte intempestivement
+	    else if(ret_send == 0) {   
+	      ret_shutdown = shutdown(fd_circuitV, SHUT_RDWR);
+	      if(ret_shutdown == -1) perror("shutdown disconnect ");
+
+	      // Ferme le descripteur de la socket
+	      ret_close = close(fd_circuitV);
+	      if(ret_close == -1) perror("close ");
+				
+	      nb_clients--;
+	      online = 0;
+	    }
+
+	    s_total_size += ret_send;
+	  }
+	  printf("file list sent\n");
+	  break;
+
+	case GET :
+	  break;
+	}
+	
+	r_total_size = 0;
+      }
+		
+    }
 	
   }
   
@@ -176,4 +212,135 @@ int main(int argc, char* argv[]) {
 
   
   return EXIT_SUCCESS;
+}
+
+struct f_list* listdir(char* path_to_dir) {
+
+  DIR* dir;
+  
+  dir = opendir(path_to_dir);
+  if(dir == NULL) {
+    perror("opendir ");
+    return NULL;
+  }
+
+  char file_list [256][256];
+  
+  char path_to_entry [256];
+  int offset = 0;
+  
+  struct dirent* entry;
+  char* entry_infos;
+  char cat_buffer [256];
+  
+  while((entry = readdir(dir)) != NULL) {
+    strcpy(path_to_entry, path_to_dir);
+    strcat(path_to_entry, "/");
+    strcat(path_to_entry, entry->d_name);
+
+    entry_infos = lstattoa(path_to_entry);
+    if(entry_infos == NULL) {
+      perror("lstattoa ");
+
+    strcpy(cat_buffer, " ");
+    strcpy(entry->d_name, strcat(cat_buffer, entry->d_name));
+    
+          
+      strcpy(cat_buffer, "No infos ");
+      strcpy(entry_infos, strcat(cat_buffer, entry->d_name));
+    } else strcpy(entry_infos, strcat(entry_infos, entry->d_name));
+    
+    file_list[offset] = entry_infos;
+
+    offset++;
+  }
+
+  struct f_list* fl = malloc(sizeof(*fl));
+  fl->file_nb = offset;
+  fl->list = file_list;
+  
+  return fl; 
+}
+
+char* lstattoa(char* path_to_file){
+  
+  struct stat file;
+  char* file_infos = malloc(256 * sizeof(*file_infos));
+
+  if(lstat(path_to_file, &file) < 0){
+    perror("lstat ");
+    return NULL;
+  }
+
+  // File type (d, - ou l)
+  if(S_ISREG(file.st_mode)){
+    strcpy(file_infos, "-");
+  }
+  if(S_ISDIR(file.st_mode)){
+    strcpy(file_infos, "d");
+  }
+  if(S_ISLNK(file.st_mode)){
+    strcpy(file_infos, "l");
+  }
+  
+  // USR permissions tests
+  if((file.st_mode & S_IRUSR) == S_IRUSR){
+    strcpy(file_infos, strcat(file_infos, "r"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IWUSR) == S_IWUSR){
+    strcpy(file_infos, strcat(file_infos, "w"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IXUSR) == S_IXUSR){
+    strcpy(file_infos, strcat(file_infos, "x"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  // GRP permissions tests
+  if((file.st_mode & S_IRGRP) == S_IRGRP){
+    strcpy(file_infos, strcat(file_infos, "r"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IWGRP) == S_IWGRP){
+    strcpy(file_infos, strcat(file_infos, "w"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IXGRP) == S_IXGRP){
+    strcpy(file_infos, strcat(file_infos, "x"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  // OTH permissions tests
+  if((file.st_mode & S_IROTH) == S_IROTH){
+    strcpy(file_infos, strcat(file_infos, "r"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IWOTH) == S_IWOTH){
+    strcpy(file_infos, strcat(file_infos, "w"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+  if((file.st_mode & S_IXOTH) == S_IXOTH){
+    strcpy(file_infos, strcat(file_infos, "x"));
+  }
+  else strcpy(file_infos, strcat(file_infos, "-"));
+
+
+  // add white space
+  strcpy(file_infos, strcat(file_infos, " "));
+
+  char file_size [10];
+  int ret_sprintf = 0;
+
+  ret_sprintf = sprintf(file_size, "%d", (int)file.st_size);
+  if(ret_sprintf == -1) perror("sprintf ");
+  else strcpy(file_infos, strcat(file_infos, file_size));
+  
+  return file_infos;
 }
